134b385415f8eb4e1ecca5f9072c375da72bd015
abeborker <abeborker@gmail.com>
2025-11-30 11:28:37 +0000

    Deploying to gh-pages from @ abeborker/abeborker.github.io@cc9bfd71cffe9bed6a6c07b812fd430bf9968e30 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,26 +16,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
